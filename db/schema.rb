c93b679b90f20a2af52d5283554c66d47b936639
ksomville <kirgan.somville@hetic.net>
2014-12-23 01:32:49 +0100

    design appli

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141219104719) do
+ActiveRecord::Schema.define(version: 20141219134609) do
 
   create_table "movie_properties", force: true do |t|
     t.integer  "user_id"
@@ -40,8 +40,6 @@ ActiveRecord::Schema.define(version: 20141219104719) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
